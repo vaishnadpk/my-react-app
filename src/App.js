a7345c242deb15c789a43a71d7256076895c8297
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-11 09:23:14 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./App.css";
+import React, { useState } from "react";
+import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [city, setCity] = useState(null);
+  const [canShowTemp, setCanShowTem] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCanShowTem(city.length > 0);
+  }
+  function handleSearch(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  function showWeather() {
+    if (canShowTemp) {
+      return <Weather city={city} />;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="label">
+        <em>
+          <a
+            href="https://www.smhi.se/en/weather/q/Stockholm/2673730"
+            target="_blank"
+            rel="noreferrer"
+            className="headspace"
+          >
+            Stockholm
+          </a>
+
+          <a
+            href="https://www.smhi.se/en/weather/q/G%C3%B6teborg/2711537"
+            target="_blank"
+            rel="noreferrer"
+            className="headspace"
+          >
+            Göteborg
+          </a>
+          <a
+            href="https://www.smhi.se/en/weather/q/Malm%C3%B6/2692969"
+            target="_blank"
+            rel="noreferrer"
+            className="headspace"
+          >
+            Malmö
+          </a>
+          <a
+            href="https://www.smhi.se/en/weather/q/Lund/2693678"
+            target="_blank"
+            rel="noreferrer"
+            className="headspace"
+          >
+            Lund
+          </a>
+        </em>
+      </div>
+      <form id="inputForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a city"
+          className="citysearch"
+          autocomplete="off"
+          autofocus="on"
+          id="searchcity"
+          onChange={handleSearch}
+        />
+        <button type="submit" className="btn btn-info" value="search">
+          Search
+        </button>
+        <button type="button" id="currentbtn" className="btn btn-success">
+          Current
+        </button>
+      </form>
+      {showWeather()}
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+export default function Weather(props) {
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  useEffect(() => {
+    let key = "453efefad2a2c05ta857fb9cb92548o9";
+    let url = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${key}&units=metric`;
+    axios
+      .get(url)
+      .then(showWeather)
+      .catch(() => {
+        console.log("Error");
+      });
+  });
+
+  function showWeather(response) {
+    setTemperature(response.data.temperature.current);
+    setDescription(response.data.condition.description);
+    setIcon(response.data.condition.icon_url);
+  }
+
+  return (
+    <div className="Weather">
+      <h1 id="cityName">{props.city}</h1>
+      <h3>
+        <span id="dayAndTime">Friday 13:57</span>
+      </h3>
+      <h4 id="description">{description}</h4>
+
+      <div className="grid">
+        <div className="row">
+          <div className="col-6">
+            <h1>
+              <img src={icon} alt="weather icon" />
+              <span id="temperature">{Math.round(temperature)}</span>{" "}
+              <sup id="celsius" className="degrees">
+                °C|
+              </sup>
+              <sup id="fahrenheit" className="degrees">
+                °F
+              </sup>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
